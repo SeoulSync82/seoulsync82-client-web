fc41b499466ce42961f318bfbbf0a00bb85bced3
henry <redbee99@naver.com>
2024-01-20 21:49:17 +0900

    🔧 (chore): 리액트 쿼리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import '@/assets/styles/index.scss';
 import { Provider } from 'react-redux';
 import { Reducer, compose, createStore } from 'redux';
 import rootReducer, { RootState } from '@/reducers/index.ts';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 declare global {
@@ -12,16 +13,21 @@ declare global {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer as Reducer<RootState, { type: string; payload: string }>,
   composeEnhancers(),
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
