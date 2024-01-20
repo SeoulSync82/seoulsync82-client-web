0e88309095bebbdff9176f23b117c0a406dd1154
henry <redbee99@naver.com>
2024-01-20 21:41:50 +0900

    🔧 (chore): redux 추가 완료

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/styles/index.scss';
+import { Provider } from 'react-redux';
+import { Reducer, compose, createStore } from 'redux';
+import rootReducer, { RootState } from '@/reducers/index.ts';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer as Reducer<RootState, { type: string; payload: string }>,
+  composeEnhancers(),
+);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
