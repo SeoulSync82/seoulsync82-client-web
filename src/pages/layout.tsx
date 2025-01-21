d7ec54e7ef32f885ef0eef9b6ba7b84ca6afc6e7
zzznly <zzznly@gmail.com>
2025-01-21 20:29:59 +0900

    mod: Layout 수정

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import Header from '@/components/header/Header';
+import Navigation from '@/components/navigation/Navigation';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <>
       <main>
         <Outlet />
+        <Navigation />
       </main>
     </>
   );
