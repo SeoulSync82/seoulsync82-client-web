9b9c92e1528c085c5527fe879a4a5a5e0b70235c
zzznly <zzznly@gmail.com>
2025-01-21 21:59:40 +0900

    deploy test

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <p className="text-20 text-red-500">Home page 입니다.</p>
+      <p className="text-20 text-red-500">Home page!!</p>
     </div>
   );
 };
