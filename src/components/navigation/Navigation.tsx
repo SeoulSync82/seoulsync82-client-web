990ff4fc226709eea25d2a845499b8aec2cf4d4e
zzznly <zzznly@gmail.com>
2025-01-24 23:09:02 +0900

    mod: Navigation 스타일 수정, 타입 수정

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import SVGIcon from '../svg-icon/SVGIcon';
 import { Link, useLocation } from 'react-router-dom';
-import { IconType } from '../button/types';
+import { NavbarIcon } from '../button/types';
 
 export default function Navigation() {
   const { pathname } = useLocation();
@@ -8,41 +8,58 @@ export default function Navigation() {
   type MenuItem = {
     name: string;
     size: number;
+    path: string;
   };
-  const menuList: Record<IconType, MenuItem> = {
-    Home: { name: '홈', size: 24 },
-    MyCourse: { name: '내 코스', size: 24 },
-    AiRecommend: { name: 'AI 추천', size: 66 },
-    Community: { name: '커뮤니티', size: 24 },
-    Profile: { name: '마이페이지', size: 24 },
+  const navbarMenu: Record<NavbarIcon, MenuItem> = {
+    Home: { name: '홈', size: 24, path: '/' },
+    MyCourse: { name: '내 코스', size: 24, path: '/my-course' },
+    AiRecommend: { name: 'AI 추천', size: 66, path: '/ai-recommend' },
+    Community: { name: '커뮤니티', size: 24, path: '/community' },
+    MyPage: { name: '마이페이지', size: 24, path: '/my-page' },
   };
 
   return (
-    <nav className="fixed bottom-0 left-0 z-50 h-[86px] w-full transition-all">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 mx-auto flex h-[109px] w-full max-w-[430px] items-end transition-all">
       <div className="h-[86px] w-full rounded-t-[20px] bg-white shadow-[0_-4px_8px_rgba(0,0,0,0.08)]">
-        <ul className="max-container flex h-full items-center justify-between py-2.5">
-          {Object.entries(menuList).map(([key, { name, size }]: [string, MenuItem]) => (
+        <ul className="justify-between] max-container flex h-full items-center">
+          {Object.entries(navbarMenu).map(([key, { name, size, path }]: [string, MenuItem]) => (
             <li
-              className={`group flex grow cursor-pointer flex-col items-center justify-center ${pathname === key ? 'active' : ''}`}
+              className={`group flex grow cursor-pointer flex-col items-center justify-center ${pathname === key && 'active'}`}
               key={key}
             >
-              <Link
-                to={key}
-                className="flex min-w-14 max-w-14 flex-col items-center justify-center"
-              >
-                <div className="w-fit">
-                  <SVGIcon
-                    name={key as IconType}
-                    width={size}
-                    height={size}
-                    active={pathname === key}
-                    color={pathname === key ? 'primary-800' : 'gray-300'}
-                  />
-                </div>
-                <div className="group-[&.active]:text-primary-800 mt-[6px] w-fit text-[12px] font-bold text-gray-300">
-                  {name}
-                </div>
-              </Link>
+              {key === 'AiRecommend' ? (
+                <Link
+                  to={path}
+                  className="flex min-w-14 max-w-14 flex-col items-center justify-center"
+                >
+                  <div className="mb-[6px] w-fit">
+                    <SVGIcon
+                      name={key as NavbarIcon}
+                      height={size}
+                      width={size}
+                      active={pathname === key}
+                    />
+                  </div>
+                  <div className="mb-8 w-fit text-12 font-bold text-[#9070CF]">{name}</div>
+                </Link>
+              ) : (
+                <Link
+                  to={path}
+                  className="flex min-w-14 max-w-14 flex-col items-center justify-center"
+                >
+                  <div className="w-fit">
+                    <SVGIcon
+                      name={key as NavbarIcon}
+                      height={size}
+                      width={size}
+                      active={pathname === key}
+                    />
+                  </div>
+                  <div className="mt-[6px] w-fit text-12 font-bold text-gray-300 group-[&.active]:text-[#353D4A]">
+                    {name}
+                  </div>
+                </Link>
+              )}
             </li>
           ))}
         </ul>
