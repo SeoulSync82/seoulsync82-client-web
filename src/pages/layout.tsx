1bc16d378a7e596d31dc9a4dc21d2c252458faff
zzznly <zzznly@gmail.com>
2025-01-24 23:09:49 +0900

    style: MainLayout 스타일 수정

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,15 +2,12 @@ import Header from '@/components/header/Header';
 import Navigation from '@/components/navigation/Navigation';
 import { Outlet } from 'react-router-dom';
 
-const Layout = () => {
+export default function MainLayout() {
   return (
-    <>
-      <main>
-        <Outlet />
-        <Navigation />
-      </main>
-    </>
+    <div className="mx-auto flex h-screen w-full max-w-[430px] flex-col bg-white shadow-[0px_0px_16px_rgba(50,_50,_50,_0.12)]">
+      <Header />
+      <Outlet />
+      <Navigation />
+    </div>
   );
-};
-
-export default Layout;
+}
