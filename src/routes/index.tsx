102116bb16d3ec6f91fbae370f6cd50b2cf63e8c
zzznly <zzznly@gmail.com>
2025-01-26 01:15:13 +0900

    mod: router, layout 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "@/layouts";
-import Home from "@/pages/home/home";
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from '@/layouts';
+import Home from '@/pages/home';
+import Login from '@/pages/login';
+import DefaultLayout from '@/layouts/default';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: '/login',
+    element: <DefaultLayout />,
+    children: [{ index: true, element: <Login /> }],
   },
 ]);
