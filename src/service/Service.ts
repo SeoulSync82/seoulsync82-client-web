5cb26f7892ff3def1ef4b753e2ae5413b43dc367
zzznly <zzznly@gmail.com>
2025-01-27 18:41:30 +0900

    mod: 401, 403 에러 케이스 분리

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -39,25 +39,31 @@ export default class Service {
     return response.data;
   }
   private async handleResponseError(error: any) {
-    const errorStatus = error.response?.status;
-
-    if (errorStatus === 401 || errorStatus === 403) {
-      let token = getAccessToken();
-      if (!token || (errorStatus === 401 && checkTokenExpired(token))) {
-        try {
-          const response = await this.silentTokenRefresh();
-          token = response.data.eid_access_token;
-          setAccessToken(token as string);
-          this.service.defaults.headers.Authorization = `Bearer ${token}`;
-        } catch (refreshError) {
-          removeAccessToken();
-          window.location.href = '/login';
+    switch (error.response?.status) {
+      case 401: {
+        let token = getAccessToken();
+        if (!token || checkTokenExpired(token)) {
+          try {
+            const response = await this.silentTokenRefresh();
+            token = response.data.eid_access_token;
+            setAccessToken(token as string);
+            this.service.defaults.headers.Authorization = `Bearer ${token}`;
+          } catch (refreshError) {
+            removeAccessToken();
+            window.location.href = '/login';
+          }
         }
+        break;
       }
-    } else if (errorStatus === 404 || errorStatus === 500) {
-      // TODO: redirect to error page
-    } else {
-      console.error('Response error:', error);
+      case 403:
+        window.location.href = '/login';
+        break;
+      case 404:
+      case 500:
+        // TODO: redirect to error page
+        break;
+      default:
+        console.error('Response error:', error);
     }
 
     return Promise.reject(error);
