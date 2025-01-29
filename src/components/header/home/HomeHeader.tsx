5664c29d75b74a34b717ebd899aaf59e32f10f23
zzznly <zzznly@gmail.com>
2025-01-29 21:47:18 +0900

    mod

--- src/components/header/home/HomeHeader.tsx
+++ src/components/header/home/HomeHeader.tsx
@@ -1,7 +1,9 @@
 import SVGIcon from '@/components/svg-icon/SVGIcon';
-import { Link } from 'react-router';
+import { useNavigate } from 'react-router';
 
 export default function HomeHeader() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex h-full w-full items-center justify-between">
       <div className="flex items-center gap-[8px]">
@@ -9,9 +11,14 @@ export default function HomeHeader() {
         <SVGIcon width={72} height={32} name="HeaderTypo" />
       </div>
       <div className="flex max-w-[72px] items-center gap-[8px]">
-        <Link to="/notifications">
-          <SVGIcon className="mr-2" width={32} height={32} name="Alarm" active={false} />
-        </Link>
+        <SVGIcon
+          className="mr-2"
+          width={32}
+          height={32}
+          name="Alarm"
+          active={false}
+          onClick={() => navigate('/notifications')}
+        />
         <SVGIcon width={32} height={32} name="Search" />
       </div>
     </div>
