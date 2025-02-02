6c66e2585e3cc58c4e6f1d724c311eceb62ad9ea
zzznly <zzznly@gmail.com>
2025-02-03 00:45:57 +0900

    refactor: useQueryParams hook 생성

---
+++ src/hooks/useQueryParams.ts
@@ -0,0 +1,36 @@
+import { useLocation, useNavigate } from 'react-router';
+
+interface NavigateOptions {
+  replace?: boolean;
+  state?: {
+    previousPath?: string;
+  };
+}
+export function useQueryParams() {
+  const navigate = useNavigate();
+  const { pathname, search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+
+  const updateQueryParam = (key: string, value: string, navigateOptions?: NavigateOptions) => {
+    searchParams.set(key, value);
+    navigate(
+      {
+        pathname,
+        search: `?${searchParams.toString()}`,
+      },
+      navigateOptions,
+    );
+  };
+
+  const deleteQueryParam = (key: string, navigateOptions?: NavigateOptions) => {
+    searchParams.delete(key);
+    navigate(
+      {
+        pathname,
+        search: `?${searchParams.toString()}`,
+      },
+      navigateOptions,
+    );
+  };
+  return { searchParams, updateQueryParam, deleteQueryParam };
+}
--- src/pages/ai-recommend/index.tsx
+++ src/pages/ai-recommend/index.tsx
@@ -8,6 +8,16 @@ import SelectThemeView from '@/components/pages/ai-recommend/SelectThemeView';
 import { useSubwayLines, useSubwayStations } from '@/service/subway/useSubwayService';
 import { useThemesList } from '@/service/theme/useThemeService';
 import SelectCustomView from '@/components/pages/ai-recommend/SelectCustomView';
+import { useCallback } from 'react';
+import { useQueryParams } from '@/hooks/useQueryParams';
+
+export const TAB_TYPES = {
+  SUBWAY: 'subway',
+  THEME: 'theme',
+  CUSTOM: 'custom',
+};
+
+const DEFAULT_LINE_UUID = '077ff3adc0e556148bf7eeb7a0273fb9';
 
 export default function AiRecommend() {
   const navigate = useNavigate();
@@ -17,22 +27,22 @@ export default function AiRecommend() {
   const tabItems = [
     {
       label: '역주변',
-      type: 'subway',
+      type: TAB_TYPES.SUBWAY,
       disabled: undefined,
     },
     {
       label: '테마선택',
-      type: 'theme',
+      type: TAB_TYPES.THEME,
       disabled: type === 'subway',
     },
     {
       label: '커스텀',
-      type: 'custom',
+      type: TAB_TYPES.CUSTOM,
       disabled: type === 'subway' || type === 'theme',
     },
   ];
 
-  const line_uuid = searchParams.get('line_uuid') || '077ff3adc0e556148bf7eeb7a0273fb9';
+  const line_uuid = searchParams.get('line_uuid') || DEFAULT_LINE_UUID;
   const station_uuid = searchParams.get('station_uuid');
   const theme_uuid = searchParams.get('theme_uuid');
 
@@ -46,25 +56,9 @@ export default function AiRecommend() {
     theme_uuid as string,
   );
 
-  const updateQueryParam = (key: string, value: string) => {
-    searchParams.set(key, value);
-    navigate(
-      {
-        pathname,
-        search: `?${searchParams.toString()}`,
-      },
-      { state: { previousPath: `${pathname}?type=${type}` } },
-    );
-  };
-  const deleteQueryParam = (key: string) => {
-    searchParams.delete(key);
-    navigate(
-      {
-        pathname,
-        search: `?${searchParams.toString()}`,
-      },
-      { state: { previousPath: `${pathname}?type=${type}` } },
-    );
+  const { updateQueryParam, deleteQueryParam } = useQueryParams();
+  const navigateOptions = {
+    state: { previousPath: `${pathname}?type=${type}` },
   };
 
   // TODO: BottomButton disabled 스타일 적용되도록 수정
@@ -78,26 +72,27 @@ export default function AiRecommend() {
       e.preventDefault();
       return;
     }
-    updateQueryParam('type', item.type);
+    updateQueryParam('type', item.type, navigateOptions);
   };
+
   const onClickSubwayLine = (item: any) => {
-    updateQueryParam('line_uuid', item.uuid);
+    updateQueryParam('line_uuid', item.uuid, navigateOptions);
     deleteQueryParam('station_uuid');
   };
   const onClickSubwayStation = (item: any) => {
-    updateQueryParam('station_uuid', item.uuid);
+    updateQueryParam('station_uuid', item.uuid, navigateOptions);
   };
   const onClickTheme = (item: any) => {
-    updateQueryParam('theme_uuid', item.uuid);
+    updateQueryParam('theme_uuid', item.uuid, navigateOptions);
   };
   const onClickCancelTheme = () => {
-    deleteQueryParam('theme_uuid');
+    deleteQueryParam('theme_uuid', navigateOptions);
   };
   const onClickSelectButton = () => {
     if (type === 'subway' && line_uuid && station_uuid) {
-      updateQueryParam('type', 'theme');
+      updateQueryParam('type', 'theme', navigateOptions);
     } else if (type === 'theme' && line_uuid && station_uuid && theme_uuid) {
-      updateQueryParam('type', 'custom');
+      updateQueryParam('type', 'custom', navigateOptions);
     }
   };
 
