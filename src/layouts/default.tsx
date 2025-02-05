ec0647998899f2afe45ea71397ae9fd5a74527a1
zzznly <zzznly@gmail.com>
2025-02-05 21:42:48 +0900

    mod: 하단 내비바 노출 페이지 조건 수정

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -6,11 +6,15 @@ export default function Layout() {
   const { pathname } = useLocation();
 
   return (
-    <div className="bg-gray-50 overflow-hidden">
+    <div className="overflow-hidden bg-gray-50">
       <div className="mx-auto flex h-dvh w-full max-w-[430px] flex-col bg-white shadow-[0px_0px_16px_rgba(50,_50,_50,_0.05)]">
         <Header />
         <Outlet />
-        {pathname === '/login' || pathname === '/ai-recommend' ? null : <Navigation />}
+        {pathname === '/login' ||
+        pathname === '/ai-recommend' ||
+        pathname.includes('/culture/') ? null : (
+          <Navigation />
+        )}
       </div>
     </div>
   );
