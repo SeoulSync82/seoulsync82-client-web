828326b92369b67ba1cd54c5bb3a0000c93fab2a
zzznly <zzznly@gmail.com>
2025-02-07 00:19:05 +0900

    mod: 전시팝업 링크 수정

--- src/pages/culture/index.tsx
+++ src/pages/culture/index.tsx
@@ -82,7 +82,7 @@ export default function Culture() {
             key={`tab-${idx}`}
             title={item.label}
             active={type === item.type}
-            href={`${pathname}?type=${item.type}`}
+            href={`${pathname}?type=${item.type}&order=${order}`}
             className={clsx('flex-1')}
           />
         ))}
