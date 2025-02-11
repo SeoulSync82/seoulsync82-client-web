ef002679d0b56c0007ee741446a2f4195fee348d
zzznly <zzznly@gmail.com>
2025-02-11 21:12:06 +0900

    fix: redux provider 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/styles/index.css';
-import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers/index.ts';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const store = configureStore({
-  reducer: rootReducer,
-});
-
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
 );
