a441b542948b7e427afe65668d4ba31fc1789e84
zzznly <zzznly@gmail.com>
2025-02-24 04:27:32 +0900

    feat: PortalContext GlobalProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/styles/index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalPortal } from './components/GlobalPortal.tsx';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <GlobalPortal.Provider>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </GlobalPortal.Provider>,
 );
