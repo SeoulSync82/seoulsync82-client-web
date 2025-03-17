f42576a6a175798025ae7c7034a90be6ee781c23
zzznly <zzznly@gmail.com>
2025-03-18 03:15:15 +0900

    refactor: Provider 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { useAuthHandler } from './hooks/useAuthHandler';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalPortal } from './components/GlobalPortal';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   useAuthHandler();
 
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <GlobalPortal.Provider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </GlobalPortal.Provider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@/assets/styles/index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { GlobalPortal } from './components/GlobalPortal.tsx';
 
-const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <GlobalPortal.Provider>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </GlobalPortal.Provider>,
-);
+root.render(<App />);
