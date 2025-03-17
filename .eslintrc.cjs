c951991c0e71051dbfe387e0018150fcc3a67d2e
zzznly <zzznly@gmail.com>
2025-03-17 17:55:17 +0900

    chore: storybook 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:tailwindcss/recommended",
     "plugin:prettier/recommended",
+    "plugin:storybook/recommended"
   ],
   overrides: [
     {
