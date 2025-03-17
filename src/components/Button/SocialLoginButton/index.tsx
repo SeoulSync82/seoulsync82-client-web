1af33cd8cbe613efda367264926a905b301d7aa6
zzznly <zzznly@gmail.com>
2025-03-18 03:14:55 +0900

    chore: 색상 변수 props 수정

--- src/components/Button/SocialLoginButton/index.tsx
+++ src/components/Button/SocialLoginButton/index.tsx
@@ -12,24 +12,16 @@ const snsStyles: Record<
     textColor: ButtonVariantsProps['textColor'];
   }
 > = {
-  kakao: { label: '카카오톡', bgColor: 'kakaoYellow', textColor: 'gray900' },
-  naver: { label: '네이버', bgColor: 'naverGreen', textColor: 'white' },
-  google: { label: '구글', bgColor: 'googleGray', textColor: 'gray900' },
+  kakao: { label: '카카오톡', bgColor: 'kakao-yellow', textColor: 'gray-900' },
+  naver: { label: '네이버', bgColor: 'naver-green', textColor: 'white' },
+  google: { label: '구글', bgColor: 'google-gray', textColor: 'gray-900' },
 };
 
 export default function SocialLoginButton({ snsName, ...rest }: SocialLoginButtonProps) {
   const { label, bgColor, textColor } = snsStyles[snsName];
 
   return (
-    <Button
-      fullWidth
-      height={48}
-      rounded={8}
-      borderColor="none"
-      bgColor={bgColor}
-      textColor={textColor}
-      {...rest}
-    >
+    <Button fullWidth height={48} rounded={8} bgColor={bgColor} textColor={textColor} {...rest}>
       <SvgIcon name={snsName} width={22} height={22} />
       <span className="ml-2 font-bold">{label} 로그인</span>
     </Button>
