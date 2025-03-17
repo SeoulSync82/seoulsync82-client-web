21e0d6996b14e3d7e5740328a953081df2cba03a
zzznly <zzznly@gmail.com>
2025-03-18 02:10:20 +0900

    refactor: SelectSubwayView 수정

--- src/components/pages/ai-recommend/SelectSubwayView.tsx
+++ src/components/pages/ai-recommend/SelectSubwayView.tsx
@@ -24,11 +24,10 @@ export default function SelectSubwayView({
 
   return (
     <div className="flex h-[calc(100vh-162px)] w-full bg-white">
-      <div className="hide-scroll flex grow basis-1/3 flex-col overflow-y-auto bg-gray-100">
+      <div className="hide-scroll flex basis-1/3 flex-col overflow-y-auto bg-gray-100">
         {subwayLineData?.data?.items.map((item) => (
           <SelectSubwayButton
             key={item.uuid}
-            // textColor={lineUuid === item.uuid ? 'primary' : 'gray-400'}
             active={lineUuid === item.uuid}
             onClick={() => onClickSubwayLine(item)}
           >
@@ -36,15 +35,12 @@ export default function SelectSubwayView({
           </SelectSubwayButton>
         ))}
       </div>
-      <div className="hide-scroll flex grow basis-2/3 flex-col overflow-y-auto">
+      <div className="hide-scroll flex basis-2/3 flex-col overflow-y-auto">
         {subwayStationData?.data?.items.map((item) => (
           <SelectStationButton
             key={item.station_uuid}
-            bgColor={stationUuid === item.uuid ? 'primary' : 'white'}
-            textColor={stationUuid === item.uuid ? 'white' : 'gray-400'}
             active={stationUuid === item.uuid}
             onClick={() => onClickSubwayStation(item)}
-            className="border-b border-gray-200 text-16 font-normal"
           >
             {item.station}
           </SelectStationButton>
