ea637a994bf5a0787d9397f564581425179e4bc7
zzznly <zzznly@gmail.com>
2025-03-19 03:46:01 +0900

    fix: staging 서버 alias 인식 못하는 이슈 수정 (@)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tsconfigPaths(),
     svgr({
       svgrOptions: {
         icon: true,
@@ -34,11 +36,6 @@ export default defineConfig({
       include: '**/*.svg',
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   server: {
     host: '127.0.0.1',
     port: 7011,
