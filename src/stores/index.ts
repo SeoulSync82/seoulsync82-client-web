64dad0903542f7b3173ab8729dc01c6827115cc3
zzznly <zzznly@gmail.com>
2025-03-18 23:31:29 +0900

    refactor: course slice 내 코스추천 관련 데이터 객체 형태로 수정(customCourseData)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,11 @@
 import { create } from 'zustand';
 import { createCourseSlice, CourseSlice } from './slices/course';
+import { createUserSlice } from './slices/user';
 
 export type AppState = CourseSlice;
 
+// TODO: zustand immer 사용
 export const useBoundStore = create<AppState>()((...args) => ({
   ...createCourseSlice(...args),
+  ...createUserSlice(...args),
 }));
