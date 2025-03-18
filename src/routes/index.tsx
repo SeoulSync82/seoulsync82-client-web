ef911e17afb9ec7e1012ab0ed01c756fc23e1d77
zzznly <zzznly@gmail.com>
2025-03-19 03:14:05 +0900

    refactor: routes 페이지이름 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
 import React, { Suspense } from 'react';
-import { createBrowserRouter, Navigate, Outlet, ScrollRestoration } from 'react-router';
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouteObject,
+  ScrollRestoration,
+} from 'react-router';
 import Layout from '@/layouts/default';
 import Loading from '@/components/Loading';
 import { authLoader } from './loader';
@@ -8,7 +14,7 @@ const Home = React.lazy(() => import('@/pages/Home'));
 const MyPage = React.lazy(() => import('@/pages/MyPage'));
 const MyCourse = React.lazy(() => import('@/pages/Course'));
 const CourseDetail = React.lazy(() => import('@/pages/Course/CourseDetail'));
-const AiRecommend = React.lazy(() => import('@/pages/AiRecommend'));
+const AiRecommendPage = React.lazy(() => import('@/pages/AiRecommendPage'));
 const Culture = React.lazy(() => import('@/pages/Culture'));
 const CultureDetail = React.lazy(() => import('@/pages/Culture/CultureDetail'));
 const Notifications = React.lazy(() => import('@/pages/Notifications'));
@@ -27,7 +33,7 @@ const Root = () => {
   );
 };
 
-const routes = [
+const routes: RouteObject[] = [
   {
     element: <Root />,
     children: [
@@ -41,7 +47,7 @@ const routes = [
         element: <CourseDetail />,
         loader: authLoader,
       },
-      { path: '/ai-recommend', element: <AiRecommend /> },
+      { path: '/ai-recommend', element: <AiRecommendPage /> },
       {
         path: '/culture',
         element: <Culture />,
