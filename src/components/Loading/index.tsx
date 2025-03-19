3ea770b8cdf7e5a6cb9affc8940a5129a882b68c
zzznly <zzznly@gmail.com>
2025-03-19 23:37:31 +0900

    refactor: Loading 컴포넌트 props 수정

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,11 +3,11 @@ import { BeatLoader } from 'react-spinners';
 interface LoadingProps {
   color?: string;
   size?: number;
-  isLoading: boolean;
+  isLoading?: boolean;
   // type?: 'bounce' | 'beat'; // TODO: type 추가
 }
 
-const Loading = ({ color = '#9070CF', size = 15, isLoading }: LoadingProps) => {
+const Loading = ({ color = '#9070CF', size = 15, isLoading = true }: LoadingProps) => {
   return (
     <div className="flex h-full items-center justify-center">
       <BeatLoader color={color} size={size} loading={isLoading} />
