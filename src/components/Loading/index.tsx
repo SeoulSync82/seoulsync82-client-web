0452d5bd903031122602ce2df25a22b2b304a693
zzznly <zzznly@gmail.com>
2025-03-19 19:21:22 +0900

    feat: Loading spinner 설정 (react-spinners)

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,4 +1,17 @@
-const Loading = () => {
-  return <div>Loading...</div>;
+import { BeatLoader } from 'react-spinners';
+
+interface LoadingProps {
+  color?: string;
+  size?: number;
+  isLoading: boolean;
+  // type?: 'bounce' | 'beat'; // TODO: type 추가
+}
+
+const Loading = ({ color = '#9070CF', size = 15, isLoading }: LoadingProps) => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <BeatLoader color={color} size={size} loading={isLoading} />
+    </div>
+  );
 };
 export default Loading;
