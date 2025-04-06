7c3582672c2f94041bc2d757a7a63239c0a061f5
zzznly <zzznly@gmail.com>
2025-04-07 04:19:54 +0900

    refactor: Modal 컴포넌트 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,39 +1,37 @@
 import { cn } from '@/utils/tailwindcss';
 import { GlobalPortal } from '@/context/GlobalPortal';
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 
 export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
   isOpen: boolean;
   onClose: () => void;
   onConfirm: () => void;
-  renderActions?: () => React.ReactNode;
+  renderActions?: () => ReactNode;
 }
 
 const Modal = ({ isOpen, onClose, onConfirm, renderActions, children, ...rest }: ModalProps) => {
   if (!isOpen) return null;
+
   return (
     <GlobalPortal.Consumer>
-      <Modal.Backdrop onClick={onClose} />
-      <Modal.Content {...rest}>{children}</Modal.Content>
+      <ModalBackdrop onClick={onClose} />
+      <ModalContent {...rest}>{children}</ModalContent>
       {renderActions?.()}
     </GlobalPortal.Consumer>
   );
 };
+
+const ModalContent = ({ children, className, ...rest }: Partial<ModalProps>) => (
+  <div {...rest} className={cn('mx-auto flex', className)}>
+    {children}
+  </div>
+);
+
+const ModalBackdrop = ({ onClick }: { onClick?: () => void }) => (
+  <div
+    className="max-container fixed inset-0 cursor-pointer bg-[rgba(0,0,0,0.6)]"
+    onClick={onClick}
+  />
+);
+
 export default Modal;
-
-Modal.Content = ({ children, className, ...rest }: Partial<ModalProps>) => {
-  return (
-    <div {...rest} className={cn('z-999 mx-auto flex', className)}>
-      {children}
-    </div>
-  );
-};
-
-Modal.Backdrop = ({ onClick }: { onClick?: () => void }) => {
-  return (
-    <div
-      className="z-100 max-container fixed bottom-0 left-0 right-0 top-0 bg-[rgba(0,0,0,0.6)] cursor-pointer"
-      onClick={onClick}
-    />
-  );
-};
