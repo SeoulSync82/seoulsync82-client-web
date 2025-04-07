ed8ef27c9185beac267e1f289cb3f48b738e001a
zzznly <zzznly@gmail.com>
2025-04-07 16:06:19 +0900

    fix: 코스상세 수정된 데이터 스키마 적용

--- src/pages/CourseDetailPage.tsx
+++ src/pages/CourseDetailPage.tsx
@@ -9,13 +9,13 @@ const CourseDetailPage = () => {
   return (
     <div className="w-full">
       <NaverMap
-        latitude={data?.data?.items.places[0].latitude}
-        longitude={data?.data?.items.places[0].longitude}
+        latitude={data?.data?.places?.[0]?.latitude}
+        longitude={data?.data?.places?.[0]?.longitude}
         height={198}
       />
       <div className="h-full w-full rounded-t-[8px] bg-white px-[20px] pt-[12px] shadow-[2px_2px_12px_0px_rgba(0,0,0,0.2)]">
         <div className="flex items-center gap-[4px]">
-          {data?.data?.items.line?.map(({ line, uuid }: { line: string; uuid: string }) => (
+          {data?.data?.line?.map(({ line, uuid }: { line: string; uuid: string }) => (
             <div
               className="flex h-[20px] items-center rounded-[50px] border-[1px] border-[#E0D1FF] px-[8px] text-10 font-semibold text-primary-500"
               key={uuid}
@@ -26,8 +26,8 @@ const CourseDetailPage = () => {
         </div>
         <div className="mt-[12px] h-full">
           <div className="text-16">
-            <span className="font-bold">{data?.data?.items.course_name.split(',')[0]}</span>
-            <span className="font-medium">,{data?.data?.items.course_name.split(',')[1]}</span>
+            <span className="font-bold">{data?.data?.course_name.split(',')[0]}</span>
+            <span className="font-medium">,{data?.data?.course_name.split(',')[1]}</span>
           </div>
         </div>
         <div>작업중...</div>
