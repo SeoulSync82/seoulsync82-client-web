fc5f99ba6a28ec650472857bf5f3fb0b0572ad4b
zzznly <zzznly@gmail.com>
2025-04-26 15:09:03 +0900

    refactor: 유저프로필 조회 쿼리 캐시타임 무한대 지정

--- src/service/user/UserService.ts
+++ src/service/user/UserService.ts
@@ -1,18 +1,12 @@
 import Service from '../Service';
 
 class UserService extends Service {
-  constructor() {
-    super();
-  }
-
   getUserProfile() {
     return this.service.get('/user/profile');
   }
-
   editUserProfile(data: { name: string; profile_image: string }) {
     return this.service.put('/user/profile', data);
   }
-
   userLogout() {
     return this.service.post('/auth/logout');
   }
--- src/service/user/useUserService.ts
+++ src/service/user/useUserService.ts
@@ -6,6 +6,7 @@ export const useUserProfile = ({ enabled }: { enabled: boolean }) => {
     queryKey: ['userProfile'],
     queryFn: () => UserService.getUserProfile(),
     enabled,
+    gcTime: Infinity,
   });
 };
 
