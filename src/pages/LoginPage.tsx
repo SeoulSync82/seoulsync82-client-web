f8820e30a9ccc1a8c94cf0e957fdcc79bde950b3
zzznly <zzznly@gmail.com>
2025-05-01 23:21:43 +0900

    refactor: LoginPage 컴포넌트화

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,38 +3,41 @@ import Tooltip from '@/components/Tooltip';
 import { SNSType } from '@/components/SvgIcon/type';
 import { SocialLoginButton } from '@/components/Button';
 import Service from '@/service/Service';
+import { useNavigate } from 'react-router';
 
-const SNS_LIST = ['kakao', 'naver', 'google'];
+const SNS_LIST: SNSType[] = ['kakao', 'naver', 'google'];
 
-export default function Login() {
-  const onClickLogin = (authType: (typeof SNS_LIST)[number]) => {
-    const service = new Service();
+export default function LoginPage() {
+  const navigate = useNavigate();
+  const service = new Service();
+
+  const onClickLogin = (authType: SNSType) => {
     const socialLoginUrl = `${service.service.defaults.baseURL}/auth/login/${authType}`;
-    window.location.href = socialLoginUrl;
+    navigate(socialLoginUrl);
   };
 
   return (
-    <div className="page">
-      <div className="max-container flex h-full items-center justify-center">
-        <div className="flex flex-col items-center justify-center">
-          <SvgIcon name="LogoSeoulsync" width={169} height={169} />
-          <Tooltip
-            size="small"
-            direction="bottomMiddle"
-            message="⚡️ 로그인하고 나에게 맞는 코스를 추천 받아보세요!"
-            className="mt-18 mb-4"
-          />
-          <div className="flex w-full flex-col items-center justify-center gap-3">
-            {SNS_LIST.map((snsName: (typeof SNS_LIST)[number]) => (
-              <SocialLoginButton
-                key={snsName}
-                snsName={snsName as SNSType}
-                onClick={() => onClickLogin(snsName)}
-              />
-            ))}
-          </div>
-        </div>
+    <div className="page max-container flex h-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <SvgIcon name="LogoSeoulsync" width={168} height={168} />
+        <Tooltip
+          size="small"
+          direction="bottomMiddle"
+          message="⚡️ 로그인하고 나에게 맞는 코스를 추천 받아보세요!"
+          className="mt-18 mb-4"
+        />
+        <SocialLoginSection onClickLogin={onClickLogin} />
       </div>
     </div>
   );
 }
+
+const SocialLoginSection = ({ onClickLogin }: { onClickLogin: (authType: SNSType) => void }) => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-3">
+      {SNS_LIST.map((snsName) => (
+        <SocialLoginButton key={snsName} snsName={snsName} onClick={() => onClickLogin(snsName)} />
+      ))}
+    </div>
+  );
+};
