4cb8cbfb3a9d2b533ae04b9843c5cabe1b1343db
zzznly <zzznly@gmail.com>
2025-05-01 23:54:26 +0900

    refactor: useAuthHandler 수정

--- src/hooks/useAuthHandler.ts
+++ src/hooks/useAuthHandler.ts
@@ -2,9 +2,10 @@ import { setAccessToken } from '@/utils/auth';
 import { useLayoutEffect } from 'react';
 
 export const useAuthHandler = () => {
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get('token');
+
   useLayoutEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const token = params.get('token');
     if (!token) return;
     setAccessToken(token);
     window.location.href = '/';
