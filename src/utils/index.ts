2f3719fa459a2dec59a20d7078804f5e3ecd548e
zzznly <zzznly@gmail.com>
2025-05-03 17:14:23 +0900

    refactor: convertDateToYMD 유틸함수 style 인자 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
-export function convertDateToYMD(dateString: string): string {
+export const convertDateToYMD = (dateString: string, style: 'dash' | 'dot' = 'dash'): string => {
   const date = new Date(dateString);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
-  return `${year}-${month}-${day}`;
-}
+  return style === 'dash' ? `${year}-${month}-${day}` : `${year}.${month}.${day}`;
+};
