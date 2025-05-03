9fdb604fd46d2606a2e2aaaf8e5ede9f6aa300b0
zzznly <zzznly@gmail.com>
2025-05-04 00:05:59 +0900

    refactor: usePlaceService queryOptions 수정, queries 파일 제거

--- src/service/place/usePlaceService.ts
+++ src/service/place/usePlaceService.ts
@@ -1,26 +1,38 @@
 import { useQuery } from '@tanstack/react-query';
-import { queryOptions } from './queries';
+import PlaceService from './PlaceService';
 
-export const usePlaceCulture = (size: number = 10, last_id: number = 0) => {
-  return useQuery(queryOptions.getPlaceCulture(size, last_id));
+export const usePlaceCulture = (size: number = 10, last_id?: number) => {
+  return useQuery({
+    queryKey: ['placeCulture', size, last_id],
+    queryFn: () => PlaceService.getPlaceCulture(size, last_id),
+  });
 };
 
 export const usePlaceExhibition = (
   size: number = 10,
-  last_id: number = 0,
-  order: 'latest' | 'deadline',
+  last_id?: number,
+  order: 'latest' | 'deadline' = 'latest',
 ) => {
-  return useQuery(queryOptions.getPlaceExhibition(size, last_id, order));
+  return useQuery({
+    queryKey: ['placeExhibition', size, last_id, order],
+    queryFn: () => PlaceService.getPlaceExhibition(size, last_id, order),
+  });
 };
 
 export const usePlacePopup = (
   size: number = 10,
-  last_id: number = 0,
-  order: 'latest' | 'deadline',
+  last_id?: number,
+  order: 'latest' | 'deadline' = 'latest',
 ) => {
-  return useQuery(queryOptions.getPlacePopup(size, last_id, order));
+  return useQuery({
+    queryKey: ['placePopup', size, last_id, order],
+    queryFn: () => PlaceService.getPlacePopup(size, last_id, order),
+  });
 };
 
 export const usePlaceDetail = (uuid: string) => {
-  return useQuery(queryOptions.getPlaceDetail(uuid));
+  return useQuery({
+    queryKey: ['placeDetail', uuid],
+    queryFn: () => PlaceService.getPlaceDetail(uuid),
+  });
 };
