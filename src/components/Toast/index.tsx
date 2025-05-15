3b4a057cabc38bb63acd57ca4a54fd254b5c4b1b
zzznly <zzznly@gmail.com>
2025-05-15 11:27:18 +0900

    style: 토스트 팝업 가로폭 사이즈 수정

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -7,7 +7,7 @@ interface ToastProps {
 
 const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
   return (
-    <div className="fixed bottom-24 left-0 right-0 mx-auto line-clamp-2 w-[390px] rounded bg-gray-900 px-4 py-3 text-center text-sm font-normal leading-5 text-white">
+    <div className="fixed bottom-24 left-5 right-5 mx-auto line-clamp-2 max-w-[390px] rounded bg-gray-900 px-4 py-3 text-center text-sm font-normal leading-5 text-white">
       {message}
     </div>
   );
