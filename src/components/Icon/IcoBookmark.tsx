09e33c0f2119a13a984ab4edba3b655f2a5cbfec
zzznly <zzznly@gmail.com>
2025-05-16 16:10:23 +0900

    refactor: IcoBookmark 수정 - props.color 색상 우선으로 적용되도록

--- src/components/Icon/IcoBookmark.tsx
+++ src/components/Icon/IcoBookmark.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcoBookmark = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill={props.color || 'none'}
+    viewBox="0 0 24 24"
+    {...props}
+  >
     <path
       stroke={props.color || '#6D757D'}
       strokeLinecap="round"
