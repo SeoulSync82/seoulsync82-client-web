6be1e50a4ae4bfad920153c661ea8fe440a95caf
zzznly <zzznly@gmail.com>
2025-05-17 01:24:11 +0900

    재시도 아이콘 추가

---
+++ src/components/Icon/IcoReset.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIcoReset = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}><path fill="#6D757D" d="M7.891 5.786a7.63 7.63 0 0 1 4.24-1.286.75.75 0 0 0 0-1.5 9.131 9.131 0 1 0 9.13 9.131 9.24 9.24 0 0 0-2.723-6.583h1.974a.75.75 0 0 0 0-1.5h-4.177a.75.75 0 0 0-.764.767v4.173a.75.75 0 0 0 1.5 0V6.237c1.654 1.407 2.69 3.538 2.69 5.894a7.63 7.63 0 1 1-11.87-6.345" /></svg>;
+export default SvgIcoReset;
